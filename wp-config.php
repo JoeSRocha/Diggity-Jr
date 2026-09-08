39dd830dd40c09703242e89a6e9380615e2146e6
root <root@wagedwar.com>
2026-09-07 17:38:55 -0700

    Reconcile production settings and load credentials and salts from environment

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,6 @@
 <?php
+define( 'WP_CACHE', true ); // Added by WP Rocket
+
 /**
  * The base configuration for WordPress
  *
@@ -20,33 +22,35 @@ require_once DIR_VENDOR . 'autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+define('DISABLE_WP_CRON', TRUE);
 
 define('DB_NAME',     $_ENV['DB_NAME']);
 define('DB_USER',     $_ENV['DB_USER']);
-define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+define('DB_PASSWORD',     $_ENV['DB_PASSWORD']);
 define('DB_HOST',     $_ENV['DB_HOST']);
 define('WP_HOME',     $_ENV['WP_HOME']);
 define('WP_SITEURL',  $_ENV['WP_HOME']);
 define('WP_DEBUG',    (int)$_ENV['WP_DEBUG']);
+define('WP_DEBUG_DISPLAY', false);
+ini_set('display_errors', 0);
 
 define('DB_CHARSET',  $_ENV['DB_CHARSET']);
 define('DB_COLLATE',  $_ENV['DB_COLLATE']);
 define('FS_METHOD',   $_ENV['FS_METHOD']);
 define('ENV',         $_ENV['ENV']);
-define('WP_CACHE',    (bool)$_ENV['WP_CACHE']);
-define('WPCACHEHOME', $_ENV['WPCACHEHOME']);
+define( 'WPCACHEHOME', '/opt/bitnami/apps/DiggityJr.com/wp-content/plugins/wp-super-cache/' );
 define('WP_TEMP_DIR', $_ENV['WP_TEMP_DIR']);
 $table_prefix =       $_ENV['TABLE_PREFIX'];
 
 
-define('AUTH_KEY',         ':&S*M~t29ocay2i<ERGXM0iwPG+X9-b(LSn$x896}+;2-bjxtqO~4-&i$_@Ddu=n');
-define('SECURE_AUTH_KEY',  '>[x5!hBiFAl]RzDc#ka|I;zy,g|=$lOHh9}K{_cN(/WJ6 iudg}Oz2Pgw#lsaLk-');
-define('LOGGED_IN_KEY',    '>&-Q1EFkS+j&6F+/jaK^hiA;i2k59)Uu-rw(py6->6(c(~9H=Z,E-JncJMKGO}V~');
-define('NONCE_KEY',        '|47QMUm~XRHLpO#5sL>.]{&uCi:okwxGBBRULkOVWL6DlT2@V8yt$_&B=sG/<Z-N');
-define('AUTH_SALT',        '|KcyeH8oDUf%$%S}b8X-gFNZR$<--[C5t-R}}69)izpQ+plw-|?Nt[-=Z>DB5x4k');
-define('SECURE_AUTH_SALT', '6~RFs;/<RoIS=&AeBeDY?b~smz_vBji|e?s4*=@EcON%(Jq0zRP|4+q]ur0:]<^q');
-define('LOGGED_IN_SALT',   'LTD^`lL be+l:2f&3&2kEv|w0TRsh1*Z]*SPv;EffW.Xio.g4*]U&tF?|#`;/->@');
-define('NONCE_SALT',       '^/kgxPGVn>TIh!NW|!8]`q-nOwc7.AraCf}4mLgcmmPNZ&p+)?sh%~VR,,{07~:9');
+define('AUTH_KEY', $_ENV['AUTH_KEY']);
+define('SECURE_AUTH_KEY', $_ENV['SECURE_AUTH_KEY']);
+define('LOGGED_IN_KEY', $_ENV['LOGGED_IN_KEY']);
+define('NONCE_KEY', $_ENV['NONCE_KEY']);
+define('AUTH_SALT', $_ENV['AUTH_SALT']);
+define('SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT']);
+define('LOGGED_IN_SALT', $_ENV['LOGGED_IN_SALT']);
+define('NONCE_SALT', $_ENV['NONCE_SALT']);
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
@@ -54,4 +58,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /** Sets up WordPress vars and included files. */
+define( 'DISALLOW_FILE_EDIT', true );
+define( 'DISALLOW_FILE_MODS', true );
+define( 'AUTOMATIC_UPDATER_DISABLED', true );
 require_once ABSPATH . 'wp-settings.php';
